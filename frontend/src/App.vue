7a2a174050d381da7e048abdd149fc025e2555a2
yangmingtian <yangmingtian@change.so>
2019-04-29 19:12:26 +0800

    thanks

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,44 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <!--<router-view/>-->
+    <!--<hello-world></hello-world>-->
+    <span v-bind:title="message">
+      ymtsss {{message}}
+    </span>
+    <div id="app-2">
+    <span v-bind:title="message">
+    Hover your mouse over me for a few seconds to see my dynamically bound title!
+  </span>
+    </div>
   </div>
+
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+
+  import HelloWorld from './components/HelloWorld'
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    },
+    data() {
+      return {
+        message: "page on " + new Date()
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
