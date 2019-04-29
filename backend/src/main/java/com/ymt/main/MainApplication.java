0f27b00e49a44eeb74223813b750798b186d2dbe
yangmingtian <yangmingtian@change.so>
2019-04-29 10:41:40 +0800

    spring boot init

---
+++ backend/src/main/java/com/ymt/main/MainApplication.java
@@ -0,0 +1,16 @@
+package com.ymt.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description TODO
+ * @Author yangmingtian
+ * @Date 2019/4/29
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
